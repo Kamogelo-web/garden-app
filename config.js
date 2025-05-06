9f9651bc4452d363ccc38c6a4f1b4f47b8d11dfc
Kamogelo Ndobeni <kamogelojr330@gmal.com>
2025-05-06 20:00:55 +0200

    Refactor hardcoded values in config

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+export const SEASON_RANGES = {
+    SPRING: { start: 2, end: 4 },
+    SUMMER: { start: 5, end: 7 },
+    AUTUMN: { start: 8, end: 10 },
+    WINTER: { start: 11, end: 1 }
+  };
+  
+  export const SEASONAL_ADVICE = {
+    spring: ["Start sowing annuals.", "Divide perennials."],
+    summer: ["Water early morning.", "Deadhead flowers."],
+    autumn: ["Plant bulbs.", "Rake leaves."],
+    winter: ["Protect from frost.", "Clean tools."]
+  };
